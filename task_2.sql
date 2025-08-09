00cd866e0a0155170537ee2e70f438cc22f9b872
Clinton oshinowo <clintonoshinowo0@gmail.com>
2025-08-09 15:38:32 -0600

    Fixes foreign key constraint in Orders table

--- task_2.sql
+++ task_2.sql
@@ -25,7 +25,7 @@ CREATE TABLE IF NOT EXISTS Orders (
     order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE NOT NULL,
-    FOREIGN KEY(customer_id) REFERENCES Customers(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 CREATE TABLE IF NOT EXISTS Order_Details (
